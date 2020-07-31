56595da2561b9200b1d42c206cffb8b0ac779d60
Nitin Bisht <nitin.bisht@iongroup.com>
2020-07-31 09:36:08 +0530

    Added Example Consumers for chatting

---
+++ jmsmessenger/src/main/java/com/clients/Consumer2.java
@@ -0,0 +1,45 @@
+package com.clients;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.JMSProducer;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class Consumer2 {
+  public static void main(String[] args)
+      throws NamingException, JMSException, InterruptedException {
+    ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+    InitialContext initialContext = new InitialContext();
+
+    Topic sentTopic = (Topic) initialContext.lookup("topic/msgSentTopic");
+    Topic receivedTopic = (Topic) initialContext.lookup("topic/msgReceivedTopic");
+
+    while (true) {
+      JMSContext jmsContext = connectionFactory.createContext();
+
+      JMSConsumer consumer = jmsContext.createConsumer(sentTopic);
+      TextMessage receiveText = (TextMessage) consumer.receive();
+      jmsContext.close();
+      Thread.sleep(500);
+      JMSContext jmsContextCon = connectionFactory.createContext();
+      try {
+        if (receiveText.getBooleanProperty("Active")) {
+          System.out.println(receiveText.getText());
+          JMSProducer chatProducer = jmsContextCon.createProducer();
+          TextMessage textMessage = jmsContextCon.createTextMessage(" Hello Nitin..!!");
+          chatProducer.send(receivedTopic, textMessage);
+          System.out.println("Message Sent");
+        }
+      } catch (Exception e) {
+        System.out.println(e);
+      }
+      jmsContextCon.close();
+    }
+  }
+}
--- jmsmessenger/src/main/java/com/servers/MainPublisher.java
+++ jmsmessenger/src/main/java/com/servers/MainPublisher.java
@@ -1,7 +1,42 @@
 package com.servers;
 
-public class MainPublisher {
-    public static void main(String[] args) {
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class MainPublisher {
+  public static void main(String[] args)
+      throws NamingException, JMSException, InterruptedException {
+    ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+    InitialContext initialContext = new InitialContext();
+    Topic sentTopic = (Topic) initialContext.lookup("topic/msgSentTopic");
+    Topic receivedTopic = (Topic) initialContext.lookup("topic/msgReceivedTopic");
+
+    JMSContext jmsContext = connectionFactory.createContext();
+    System.out.println("Connection established");
+    TextMessage textMessage = jmsContext.createTextMessage("Chat Begins : ");
+    textMessage.setBooleanProperty("Active", true);
+    jmsContext.createProducer().send(sentTopic, textMessage);
+    jmsContext.close();
+
+    JMSContext jmsContextCon = connectionFactory.createContext();
+    try {
+      JMSConsumer chatConsumer = jmsContextCon.createConsumer(receivedTopic);
+      TextMessage chatReceive = (TextMessage) chatConsumer.receive();
+      System.out.println(chatReceive.getText());
+
+      JMSConsumer chatConsumer1 = jmsContextCon.createConsumer(receivedTopic);
+      TextMessage chatReceive1 = (TextMessage) chatConsumer1.receive();
+      System.out.println(chatReceive1.getText());
+    } catch (Exception ex) {
+      System.out.println(ex);
     }
+    jmsContext.close();
+  }
 }
